c3b824c4ea6e8bf5f14542c0e4e13270279115b0
Henrique E. Gregorio <henriquee.gr@gmail.com>
2022-07-16 19:27:04 -0300

    controller creation

---
+++ Application/Controllers/UsersController.cs
@@ -0,0 +1,111 @@
+﻿using Domain.Interfaces.Services;
+using Domain.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Application.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : Controller
+    {
+        public IUserService _service { get; set; }
+        public UsersController(IUserService service)
+        {
+            _service = service;
+        }
+
+        [HttpDelete("(id)")]
+        public async Task<ActionResult> Delete(Guid id)
+        {
+            if(!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            try
+            {
+                return Ok(await _service.Delete(id));
+            }
+            catch (ArgumentException ex)
+            {
+                return StatusCode((int)HttpStatusCode.InternalServerError, ex.Message);
+            }
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> Get()
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            try
+            {
+                return Ok(await _service.Get());
+            }
+            catch (ArgumentException ex)
+            {
+                return StatusCode((int)HttpStatusCode.InternalServerError, ex.Message);
+            }
+        }
+
+        [HttpGet]
+        [Route("{id}", Name = "GetWithId")]
+        public async Task<ActionResult> GetDetailId(Guid id)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            try
+            {
+                return Ok(await _service.GetDetailId(id));
+            }
+            catch (ArgumentException ex)
+            {
+                return StatusCode((int)HttpStatusCode.InternalServerError, ex.Message);
+            }
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post([FromBody] UserModel userModel)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            try
+            {
+                var result = await _service.Post(userModel);
+
+                if (result != null)
+                    return Created(new Uri(Url.Link("GetWithId", new { id = result.Id })), result);
+                else
+                    return BadRequest();
+            }
+            catch (ArgumentException ex)
+            {
+                return StatusCode((int)HttpStatusCode.InternalServerError, ex.Message);
+            }
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> Put([FromBody]UserModel userModel)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            try
+            {
+                var result = await _service.Post(userModel);
+
+                if (result != null)
+                    return Ok(result);
+                else
+                    return BadRequest();
+            }
+            catch (ArgumentException ex)
+            {
+                return StatusCode((int)HttpStatusCode.InternalServerError, ex.Message);
+            }
+        }
+    }
+}
---
+++ CrossCutting/DependencyInjection/ConfigureRepository.cs
@@ -0,0 +1,20 @@
+﻿using Data.Context;
+using Data.Repository;
+using Domain.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CrossCutting.DependencyInjection
+{
+    public class ConfigureRepository
+    {
+        public static void ConfigureDependenciesRepository(IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddScoped(typeof(IBaseRepository<>), typeof(BaseRepository<>));
+
+            serviceCollection.AddDbContext<MyContext>(
+                options => options.UseMySql("Server=127.0.0.1;Port=3306;DataBase=dbAPI;Uid=root;Pwd=1234")
+            );
+        }
+    }
+}
