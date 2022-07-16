c071886ba06d9fcd18170115f3e92b0b8e270b4a
Henrique E. Gregorio <henriquee.gr@gmail.com>
2022-07-16 17:55:01 -0300

    Created the IUserService and UserService

---
+++ Domain/Interfaces/Services/IUserService.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Domain.Interfaces.Services
+{
+    public interface IUserService
+    {
+        Task<bool> Delete(Guid id);
+        Task<IEnumerable<UserModel>> Get();
+        Task<UserModel> GetDetailId(Guid id);
+        Task<UserModel> Post(UserModel userModel);
+        Task<UserModel> Put(UserModel userModel);
+    }
+}
---
+++ Service/Services/UserService.cs
@@ -0,0 +1,44 @@
+﻿using Domain.Interfaces;
+using Domain.Interfaces.Services;
+using Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Service.Services
+{
+    public class UserService : IUserService
+    {
+        private IBaseRepository<UserModel> _baseRepository;
+
+        public UserService(IBaseRepository<UserModel> baseRepository)
+        {
+            _baseRepository = baseRepository;
+        }
+
+        public async Task<bool> Delete(Guid id)
+        {
+            return await _baseRepository.DeleteAsync(id);
+        }
+
+        public async Task<IEnumerable<UserModel>> Get()
+        {
+            return await _baseRepository.SelectAllAsync();
+        }
+
+        public async Task<UserModel> GetDetailId(Guid id)
+        {
+            return await _baseRepository.SelectDetailIdAsync(id);
+        }
+
+        public async Task<UserModel> Post(UserModel userModel)
+        {
+            return await _baseRepository.InsertAsync(userModel);
+        }
+
+        public async Task<UserModel> Put(UserModel userModel)
+        {
+            return await _baseRepository.UpdateAsync(userModel);
+        }
+    }
+}
